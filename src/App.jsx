21a96cb1aa3625694fceaa3cdaa6bcc3935c198b
Ayyaz <ayyazkhira008@gmail.com>
2026-05-25 13:57:41 +0530

    Added ScrollToTop for footer links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import PassportPhoto from './pages/PassportPhoto';
 import ResizeImage from './pages/ResizeImage';
@@ -13,6 +14,7 @@ import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
